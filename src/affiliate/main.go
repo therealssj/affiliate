06fae1a3e1947f35aed8204a94b413ef5b1aefc9
lijiangt <lijiangt@gmail.com>
2017-12-19 22:53:16 +0800

    use simple web framework

--- src/affiliate/main.go
+++ src/affiliate/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"github.com/spaco/affiliate/src/tracking_code"
 
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -13,36 +21,81 @@ func main() {
 			fmt.Println("ERROR: %s", err)
 		}
 	}()
-	http.HandleFunc("/code/", codeHandler)
-    http.HandleFunc("/code/generate/", generateHandler)
-    http.HandleFunc("/code/my-invitation/", myInvitationHandler)
-    err := http.ListenAndServe(":8888", nil)
-    if err != nil {
-        println("error： %s", err)
-    }
+	listen := flag.String("-listen", ":6060", "Interface and port to listen on")
+	flag.Parse()
+	fmt.Println("Listening on", *listen)
+	log.Fatal(http.ListenAndServe(*listen, getRouter()))
 }
 
-func codeHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	ref := r.FormValue("ref")
-    renderTemplate(w, "code", struct{Ref string}{Ref: ref})
+func codeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    renderTemplate(w, "code", &struct{Ref string}{Ref: ps.ByName("ref")})
 }
 
-func generateHandler(w http.ResponseWriter, r *http.Request) {
-
+func generateHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	addr := ps.ByName("address")
+	ref := ps.ByName("ref");
+	fmt.Println("Addr: %s, Ref: %s", addr, ref)
+	code := tracking_code.GenerateCode(12)
+	renderTemplate(w, "generate", &struct{Code string}{Code: code})
 }
 
-func myInvitationHandler(w http.ResponseWriter, r *http.Request) {
-
+func myInvitationHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	renderTemplate(w, "my-invitation", &struct{Code string}{Code: "9527"})
 }
 
-var templates = template.Must(template.ParseFiles("tpl/code.html", "tpl/generate.html","tpl/my-invitation.html"))
 
+var (
+	templateMap = template.FuncMap{
+		"Upper": func(s string) string {
+			return strings.ToUpper(s)
+		},
+	}
+	templates   = template.New("").Funcs(templateMap)
+	templateBox *rice.Box
+)
 
-func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-	err := templates.ExecuteTemplate(w, "tpl/"+tmpl+".html", data)
+func newTemplate(path string, _ os.FileInfo, _ error) error {
+	if path == "" {
+		return nil
+	}
+	templateString, err := templateBox.String(path)
+	if err != nil {
+		log.Panicf("Unable to extract: path=%s, err=%s", path, err)
+	}
+	if _, err = templates.New(filepath.Join("tpl", path)).Parse(templateString); err != nil {
+		log.Panicf("Unable to parse: path=%s, err=%s", path, err)
+	}
+	return nil
+}
+
+// Render a template given a model
+func renderTemplate(w http.ResponseWriter, tmpl string, p interface{}) {
+	err := templates.ExecuteTemplate(w, "tpl/"+tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
 
+
+func getRouter() *httprouter.Router {
+	// Load and parse templates (from binary or disk)
+	templateBox = rice.MustFindBox("tpl")
+	templateBox.Walk("", newTemplate)
+
+	// mux handler
+	router := httprouter.New()
+
+	// Index routee
+	router.GET("/code/", codeHandler)
+
+	// Example route that takes one rest style option
+	router.POST("/code/generate/", generateHandler)
+
+	// Example route that encounters an error
+	router.POST("/code/my-invitation/", myInvitationHandler)
+
+	// Serve static assets via the "static" directory
+	fs := rice.MustFindBox("s").HTTPBox()
+	router.ServeFiles("/s/*filepath", fs)
+	return router
 }
